b1a3c0f0c389a787378d4059393cad933d2247f0
disensee <isenseed@students.westerntc.edu>
2020-09-08 16:46:05 -0500

    Initial commit

---
+++ project/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+	
+	var tm = namespace.CustomerModule({
+		webServiceURL: "https://dylanisensee.com/adv-topics/web-services/customers/",
+		listContainer: document.getElementById("list-container"),
+		editContainer: document.getElementById("edit-container"),
+	});
+
+
+});
